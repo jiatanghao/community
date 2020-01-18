7b4e3b42be93514f7842be802ed136788b97ed94
jiatanghao <476736586@qq.com>
2020-01-18 23:52:23 +0800

    根据登录状态修改页面显示

--- src/main/java/com/zte/oes/community/controller/AuthorizeController.java
+++ src/main/java/com/zte/oes/community/controller/AuthorizeController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -28,13 +30,16 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state, HttpServletRequest request) {
         AccessTokenDto dto = new AccessTokenDto(clientId, clientSecret, code);
         dto.setState(state);
         dto.setRedirectUri(redirectUri);
         String token = provider.getAccessToken(dto);
         GithubUser user = provider.getGithubUser(token);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null) {
+            // 登录成功，写session和cookie
+            request.getSession().setAttribute("user", user);
+        }
+        return "redirect:/";
     }
 }
