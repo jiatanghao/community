ae1395a82939cd9c6f312299195881bd6edf21be
jiatanghao <476736586@qq.com>
2020-01-18 23:00:32 +0800

    修改配置信息到配置文件中

--- src/main/java/com/zte/oes/community/controller/AuthorizeController.java
+++ src/main/java/com/zte/oes/community/controller/AuthorizeController.java
@@ -3,6 +3,7 @@ package com.zte.oes.community.controller;
 import com.zte.oes.community.dto.AccessTokenDto;
 import com.zte.oes.community.dto.GithubUser;
 import com.zte.oes.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,15 @@ public class AuthorizeController {
 
     private final GithubProvider provider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.client.redirect-uri}")
+    private String redirectUri;
+
     public AuthorizeController(GithubProvider provider) {
         this.provider = provider;
     }
@@ -19,9 +29,9 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
-        AccessTokenDto dto = new AccessTokenDto("25ebf453af2fb8dc34c8", "8ca5c4291ec823e94d89de2bb8f25a7e87994c42", code);
+        AccessTokenDto dto = new AccessTokenDto(clientId, clientSecret, code);
         dto.setState(state);
-        dto.setRedirectUri("http://localhost/callback");
+        dto.setRedirectUri(redirectUri);
         String token = provider.getAccessToken(dto);
         GithubUser user = provider.getGithubUser(token);
         System.out.println(user.getName());
